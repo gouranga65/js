2b184a93d885dd699b433f7414da6b4519fde48a
gouranga65 <gm49302@gmail.com>
2022-04-24 12:03:57 +0530

    master

---
+++ advstring.js
@@ -0,0 +1,6 @@
+var fabShow="money-hiest";
+console.log(fabShow.length);
+console.log(fabShow[5]);
+console.log(fabShow.indexOf("y"));
+console.log(fabShow.slice(6,8));
+console.log(fabShow.toUpperCase());
