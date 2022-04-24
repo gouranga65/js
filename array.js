05f39e7c604ea36f24ab0f75d6da8646606d8de8
gouranga65 <gm49302@gmail.com>
2022-04-24 21:18:08 +0530

    master

---
+++ array.js
@@ -0,0 +1,5 @@
+//  ctrl +shift +p for open QUOKKA 
+let movie =["lcpd","got","vikings","rrr","kgf"]
+console.log(movie);
+console.log(movie[2]);
+console.log(movie[1].length);
---
+++ function.js
@@ -0,0 +1,11 @@
+function vame(ok) {
+    console.log("hello-world" + ok)
+    // we can concatinate many items
+};
+vame();
+vame(" gouranga");
+
+function add(a,b) {
+    return(a+b);
+}
+console.log(add(5,5))
