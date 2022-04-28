68c3e78877621ecc78e830c8c9e094bb0b32f5de
gouranga65 <gm49302@gmail.com>
2022-04-28 19:56:56 +0530

    master

---
+++ method.js
@@ -0,0 +1,19 @@
+let arr=[312,43.4,75,"gouranga",true,-32]
+console.log(arr.length);
+arr.pop();
+console.log(arr);
+arr.push("sundar");
+console.log(arr);
+arr.shift();
+console.log(arr);
+arr.unshift(09);
+console.log(arr);
+arr.toString();
+console.log(arr);
+console.log(typeof(arr));
+arr.sort();
+console.log(arr);
+let d =[432,53,6,3,783,8]
+d.sort();
+// it is sort by alphabetically
+console.log(d);
---
+++ s_method.js
@@ -0,0 +1,7 @@
+let me ="my name is gouranga sundar mahato"
+console.log(me.length);
+// 'o' must be capital in indexOf
+console.log(me.indexOf("is")); 
+console.log(me.slice(0,5));
+let d =me.replace("my","MY");
+console.log(d);
