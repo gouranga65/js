80945a3549545c8797c9238eefac552adc1fd119
gouranga65 <gm49302@gmail.com>
2022-04-30 00:01:43 +0530

    master

---
+++ date.js
@@ -0,0 +1,5 @@
+let mydate= new Date();
+console.log(mydate);
+console.log(mydate.getTime());
+console.log(mydate.getFullYear());
+console.log(mydate.getDay());
---
+++ dom.js
@@ -0,0 +1,7 @@
+console.log("welcome to document object model");
+let a="gouranga";
+// let b=a;
+// a=gouranga.body;
+// these above code are not working
+a="gouranga".links;
+console.log(a);
