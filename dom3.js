f826a62db2103a3cead7856da03ae00fef4aeae2
gouranga65 <gm49302@gmail.com>
2022-05-01 19:00:13 +0530

    again dom but clear

---
+++ dom3.js
@@ -0,0 +1,12 @@
+console.log("code with harry_dom3");
+let a=document;
+// a = document.getElementById();
+// above not working
+// a = document.forms;
+a=document.links[0];
+
+// below not working 
+// Array.from(a).forEach(function(element) {
+//     console.log(element);
+// }
+console.log(a);
