0bbeae94e69b265bf4d5d7390a01b203b0a09924
gouranga65 <gm49302@gmail.com>
2022-05-10 15:10:18 +0530

    fibonacci series

--- programiz-js/fibonacci.js
+++ programiz-js/fibonacci.js
@@ -1,6 +1,10 @@
-let res=1;
-for (let i=0;i<3;++i){
-    res=res+i;
-    console.log(res);
+let x=0;
+let y=1;
+
+let fn=x+y;
+for(let i=0;i<10;i++){
+    console.log(x);
+    fn=x+y;
+    x=y;
+    y=fn;
 }
-// not complete yet
