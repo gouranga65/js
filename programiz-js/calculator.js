2cb835e51d3d494c105b88f11638f522054fdaa5
gouranga65 <gm49302@gmail.com>
2022-05-13 00:13:42 +0530

    done

---
+++ programiz-js/calculator.js
@@ -0,0 +1,25 @@
+let vaLue1=20;
+let vaLue2=30;
+// change the Add value on your choice
+let Add=3;
+switch (Add) {
+    case 0:
+        let additioN=vaLue1+vaLue2;
+        console.log(additioN);
+        break;
+    case 1:
+        let subsTraction=vaLue1-vaLue2;
+        console.log(subsTraction);
+        break;
+    case 2:
+        let multiPlication=vaLue1*vaLue2;
+        console.log(multiPlication);
+        break;
+    case 3:
+        let DiviSion=vaLue1/vaLue2;
+        console.log(DiviSion);
+        break;
+    default:
+        console.log("wrong choice");
+        break;
+}
