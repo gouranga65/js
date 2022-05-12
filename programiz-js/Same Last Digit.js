e6f587e1176041c0d9605b12380419e5dd800ec7
gouranga65 <gm49302@gmail.com>
2022-05-13 00:26:03 +0530

    done

---
+++ programiz-js/Same Last Digit.js
@@ -0,0 +1,12 @@
+// JavaScript Program to Check if the Numbers Have Same Last Digit
+let vaLue1=38;
+let vaLue2=9;
+let Result1=vaLue1%10;
+let Result2=vaLue2%10;
+if (Result1 == Result2) {
+    console.log("same");
+}
+else{
+    console.log("not");
+}
+// referance:-https://www.programiz.com/javascript/examples/last-digit
