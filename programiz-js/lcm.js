94b94de6f8c2681f02ce9a1bd45c508da1c1ee25
gouranga65 <gm49302@gmail.com>
2022-05-13 15:51:13 +0530

    done

--- programiz-js/lcm.js
+++ programiz-js/lcm.js
@@ -1,2 +1,9 @@
-let numBer1=3;
-let numBer2=4;
+let numBer1=12;
+let numBer2=20;
+for (let i =1;i<=numBer1*numBer2;i++){
+    if(i%numBer1==0 && i%numBer2==0){
+        console.log(i);
+        break;
+    }
+}
+// referance:-https://www.youtube.com/watch?v=swrGRiEDbhk&ab_channel=ProgrammerShakil
