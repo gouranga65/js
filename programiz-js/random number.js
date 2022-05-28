c4f0bfa75eac04b1ab05a7132bb7faed948cdc84
gouranga65 <gm49302@gmail.com>
2022-05-28 15:39:03 +0530

    done

---
+++ programiz-js/random number.js
@@ -0,0 +1,8 @@
+// enter a highest range
+let High = 100;
+// enter a lowest range
+let Low = 1;
+// generate a random number
+let ranDom = Math.random() * (High - Low) + Low;
+// print
+console.log(parseInt(ranDom));
