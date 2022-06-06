3d349020e22581fc2e749a15373fc522fa85770b
gouranga65 <gm49302@gmail.com>
2022-06-06 19:56:38 +0530

    done

---
+++ programiz-js/Shuffle Deck of Cards.js
@@ -0,0 +1,9 @@
+let suits=["h","c","d","s"];
+let ranks=["1","2","3","4","5","6","7","8","9","10","k","q","j"];
+for(let suitsCounter=0;suitsCounter<4;suitsCounter++){
+    for(let ranksCounter=0;ranksCounter<13;ranksCounter++){
+        // console.log(ranks[ranksCounter]+suits[suitsCounter]);
+        console.log(`${ranks[ranksCounter]+suits[suitsCounter]}`);
+    }
+}
+// referance:-https://www.youtube.com/watch?v=LxXWTXOny3A&t=182s&ab_channel=mrGcoding
