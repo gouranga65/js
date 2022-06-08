d1264fb19b0dc087cfb1aca3bc3dc54b8df58f47
gouranga65 <gm49302@gmail.com>
2022-06-08 09:23:26 +0530

    using recursion

--- programiz-js/Factorial of Number Using Recursion.js
+++ programiz-js/Factorial of Number Using Recursion.js
@@ -1,8 +1,17 @@
-function fibo(value) {
-  let result = 1;
-  for (i = value; i >= 1; i--) {
-    result = result * i;
+// function fibo(value) {
+//   let result = 1;
+//   for (i = value; i >= 1; i--) {
+//     result = result * i;
+//   }
+//   console.log(result);
+// }
+// fibo(5);
+function fact(numBer) {
+  if (numBer == 0) {
+    return 1;
+  } else {
+    return numBer * fact(numBer - 1);
   }
-  console.log(result);
 }
-fibo(5);
+let result = fact(5);
+console.log(result);
