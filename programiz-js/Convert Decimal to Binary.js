5755d91169ac443628c58ca66ccf5d4248153f89
gouranga65 <gm49302@gmail.com>
2022-06-28 23:53:51 +0530

    not working

--- programiz-js/Convert Decimal to Binary.js
+++ programiz-js/Convert Decimal to Binary.js
@@ -1,25 +1,13 @@
-// // let num1=27;
-// // if(num1==0){
-// //     console.log(0);
-// // }
-// // else{
-// //     let reaminder=num1%2;
-// //     let quoTient=num1/2;
-// //     num1=quoTient;
-// //     console.log(parseInt(reaminder));
-// // }
-// function Rem(x){
-// //  console.log(x);
-// if (x==0){
-//     // return 1;
-//     console.log(parseInt(1));
-// }
-// else{
-//     let reaminder=x%2;
-//     // return reaminder;
-//     console.log(reaminder);
-//     let quoTient=x/2;
-//     Rem(x)=quoTient;
-// }
-// };
-// Rem(27);
+function binary(Value){
+  if(Value==0){
+    // console.log();
+    return 0;
+  }  
+  else{
+    // Value%2?
+    return Value%2;
+    
+  }
+}
+let c= binary(27);
+console.log(c);
