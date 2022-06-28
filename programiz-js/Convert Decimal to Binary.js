dd660e4925a26ea4bc8bdd6547d3e0146abfe53c
gouranga65 <gm49302@gmail.com>
2022-06-28 11:34:18 +0530

    not done

---
+++ programiz-js/Convert Decimal to Binary.js
@@ -0,0 +1,25 @@
+// // let num1=27;
+// // if(num1==0){
+// //     console.log(0);
+// // }
+// // else{
+// //     let reaminder=num1%2;
+// //     let quoTient=num1/2;
+// //     num1=quoTient;
+// //     console.log(parseInt(reaminder));
+// // }
+// function Rem(x){
+// //  console.log(x);
+// if (x==0){
+//     // return 1;
+//     console.log(parseInt(1));
+// }
+// else{
+//     let reaminder=x%2;
+//     // return reaminder;
+//     console.log(reaminder);
+//     let quoTient=x/2;
+//     Rem(x)=quoTient;
+// }
+// };
+// Rem(27);
