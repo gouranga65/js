a5afb9a08fcabd9e4ccb4a969318b309bb2757eb
gouranga65 <gm49302@gmail.com>
2022-06-30 12:55:29 +0530

    done

---
+++ programiz-js/Convert Decimal to Binary.js
@@ -0,0 +1,9 @@
+// program to convert decimal to binary
+
+// take input
+const number = parseInt("27")
+
+// convert to binary
+const result = number.toString(2);
+
+console.log('Binary:' + ' ' + result);
