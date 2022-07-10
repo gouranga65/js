5a9241f38542cc2bdb0b77524e6b835f2d5e5923
gouranga65 <gm49302@gmail.com>
2022-07-10 10:47:20 +0530

    done

---
+++ Specified Value.js
@@ -0,0 +1,9 @@
+// JavaScript Program to Check if An Array Contains a Specified Value
+let arr = [10, 12, 53, 643, 54, 73, 75, 39, 56];
+// console.log(arr);
+for (let i = 0; i <= arr.length - 1; i++) {
+//   console.log(arr[i]);
+if (arr[i]==73) {
+    console.log("we found");
+}
+}
