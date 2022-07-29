4b59fa18b1a3fdf0feb9effc0b194f98dc72e52d
gouranga65 <gm49302@gmail.com>
2022-07-30 01:42:07 +0530

    done but in reverse

--- bubble sort.js
+++ bubble sort.js
@@ -1,5 +1,14 @@
-let Data= [6,4,0, 3,-2,1];
+let Data = [6, 4, 0, 3, -2, 1];
 for (let i = 0; i < Data.length; i++) {
-console.log(Data[i]);
-
+  if (Data[i] < Data[i + 1]) {
+    let c = Data[i + 1];
+    Data[i + 1] = Data[i];
+    Data[i] = c;
+  }
+  console.log(Data[i]);
 }
+// let a=5,b=6,c;
+// c=b;
+// b=a;
+// a=c;
+// console.log(`a is ${a} and b is ${b}`);
